c5fa7b41b40a4daacb106f7a088679f83d1c6899
Mohammed Shaik Hussain Ali <me@shaiksphre.com>
2019-05-04 03:09:19 -0700

    Separating tests for various scripts

--- test/test.sh
+++ test/test.sh
@@ -147,10 +147,12 @@ function test_function {
 #
 # Test setup_dev_env.sh
 #
-#declare -r TEST_1_FILE="${THIS_FOLDER}/../src/setup_dev_env.sh"
-#
-#case ${OS_NAME} in
-#    Darwin)
+declare -r TEST_1_FILE="${THIS_FOLDER}/../src/setup_dev_env.sh"
+
+case ${OS_NAME} in
+    Darwin)
+        echo_message --debug "THIS IS DARWIN"
+
 #        declare -ra TEST_1_DARWIN_CALLS=( "bash ${TEST_1_FILE} -h" "bash ${TEST_1_FILE}" "sudo bash ${TEST_1_FILE}" )
 #        declare -ra TEST_1_DARWIN_CODES=( 0 1 ${ERROR_RECOVERABLE} )
 #
@@ -165,8 +167,10 @@ function test_function {
 #            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 #            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 #        done
-#        ;;
-#    Linux)
+        ;;
+    Linux)
+        echo_message --debug "THIS IS LINUX"
+
 #        declare -ra TEST_1_LINUX_CALLS=( "bash ${TEST_1_FILE} -h" "bash ${TEST_1_FILE}" )
 #        declare -ra TEST_1_LINUX_CODES=( 0 ${ERROR_RECOVERABLE} )
 #
@@ -182,79 +186,6 @@ function test_function {
 #            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 #        done
 #
-#        echo_message --began "TESTING 'sudo bash ${TEST_1_FILE}'"
-#
-#        cp ${TEST_1_FILE} .
-#
-#        docker build --file ${THIS_FOLDER}/Dockerfile --tag script-demo .
-#
-#        docker run --name demo script-demo
-#
-#        STATUS_TEST="$?"
-#
-#        if [[ ${STATUS_TEST} -eq 0 ]]
-#        then
-#            echo ""
-#            printf "\033[32m+++++++++++++++++++++++\033[0m\n"
-#            echo_message --success "TEST PASSED"
-#            printf "\033[32m+++++++++++++++++++++++\033[0m\n"
-#        else
-#            echo ""
-#            printf "\033[31m+++++++++++++++++++++++\033[0m\n"
-#            echo_message --error "TEST FAILED - ${STATUS_TEST}"
-#            printf "\033[31m+++++++++++++++++++++++\033[0m\n"
-#
-#            exit ${STATUS_TEST}
-#        fi
-#
-#        echo_message --ended "TESTING 'sudo bash ${TEST_1_FILE}'"
-#        ;;
-#
-#esac
-
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# DEFAULT COMMANDS
-
-#
-# Test setup_ssh_keys.sh
-#
-declare -r TEST_2_FILE="${THIS_FOLDER}/../src/setup_ssh_keys.sh"
-
-case ${OS_NAME} in
-    Darwin)
-        declare -ra TEST_2_DARWIN_CALLS=( "bash ${TEST_2_FILE} -h" "bash ${TEST_2_FILE}" "sudo bash ${TEST_2_FILE}" )
-        declare -ra TEST_2_DARWIN_CODES=( 0 1 1 )
-
-        for (( i = 0; i < ${#TEST_2_DARWIN_CALLS[@]}; i++ ))
-        do
-            test_function "${TEST_2_DARWIN_CALLS[i]}" "${TEST_2_DARWIN_CODES[i]}"
-
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        done
-        ;;
-    Linux)
-        declare -ra TEST_2_LINUX_CALLS=( "bash ${TEST_2_FILE} -h" "bash ${TEST_2_FILE}" )
-        declare -ra TEST_2_LINUX_CODES=( 0 ${ERROR_RECOVERABLE} )
-
-        for (( j = 0; j < ${#TEST_2_LINUX_CALLS[@]}; j++ ))
-        do
-            test_function "${TEST_2_LINUX_CALLS[j]}" "${TEST_2_LINUX_CODES[j]}"
-
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        done
-
 #        echo_message --began "TESTING 'sudo bash ${TEST_1_FILE}'"
 #
 #        cp ${TEST_1_FILE} .
