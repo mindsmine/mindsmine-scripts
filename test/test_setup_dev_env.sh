7439e4b298420662d9a4f444d82a00bf4854706b
Mohammed Shaik Hussain Ali <me@shaiksphere.com>
2023-10-10 19:56:14 -0400

    Testing on linux

--- test/test_setup_dev_env.sh
+++ test/test_setup_dev_env.sh
@@ -154,8 +154,6 @@ declare -r TEST_FILE="${FOLDER_SRC}/${FILE_NAME}"
 
 case ${OS_NAME} in
     Darwin)
-        sw_vers
-
         declare -ra TEST_DARWIN_CALLS=( "bash ${FILE_NAME} -h" "bash ${FILE_NAME}" "sudo bash ${FILE_NAME}" )
         declare -ra TEST_DARWIN_CODES=( 0 0 ${ERROR_RECOVERABLE} )
 
